28677e7ebfae10f92f7b5e6637abf5f3c5421cfd
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-04-04 21:18:51 +0200

    New folder

--- My_Brand_FrontEnd/login.js
+++ My_Brand_FrontEnd/login.js
@@ -5,22 +5,30 @@ document.addEventListener("DOMContentLoaded", function() {
     const err = document.querySelector(".err");
 
     // Retrieve registered users and admin credentials from localStorage
+    // @ts-ignore
     const users = JSON.parse(localStorage.getItem("Users")) || [];
+    // @ts-ignore
     const adminCredentials = JSON.parse(localStorage.getItem("AdminCredentials")) || { email: "hitapeter2@gmail.com", password: "Hitayezu@1" };
 
+    // @ts-ignore
     loginForm.addEventListener("submit", async (event) => {
         event.preventDefault(); // Prevent form submission
         
         // Reset error message
+        // @ts-ignore
         err.style.display = "none";
 
         // Get email and password values
+        // @ts-ignore
         const email = loginEmail.value.trim();
+        // @ts-ignore
         const password = loginPassword.value.trim();
 
         // Validate email and password
         if (email === "" || password === "") {
+            // @ts-ignore
             err.textContent = "Email and password are required.";
+            // @ts-ignore
             err.style.display = "block";
             return; // Stop further execution
         }
@@ -35,8 +43,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.href = "Admin_panel/admin.html";
             } else {
                 // Display error message for non-existing email
+                // @ts-ignore
                 err.textContent = "Email or Password not found. Please register first or use admin credentials.";
+                // @ts-ignore
                 err.style.display = "block";
+                // @ts-ignore
                 err.style.color='red'
             }
             return; // Stop further execution
@@ -45,8 +56,11 @@ document.addEventListener("DOMContentLoaded", function() {
         // Check if the entered password matches the registered password
         if (password !== user.password) {
             // Display error message for incorrect password
+            // @ts-ignore
             err.textContent = "Incorrect password. Please try again.";
+            // @ts-ignore
             err.style.display = "block";
+            // @ts-ignore
             err.style.color='red'
             return; // Stop further execution
         }
